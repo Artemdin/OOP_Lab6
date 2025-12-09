1032772a38e8a7e1eb574849d1f7365fcd0a1def
Artemdin <dinovartem@gmail.com>
2025-12-09 21:52:08 +0200

    first upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Numerics;
+using System.Threading.Channels;
+
+
+namespace OOP_Lab5
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
