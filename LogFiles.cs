915b50cca8a146ea864a256af48c1eed9690129b
Artemdin <dinovartem@gmail.com>
2025-12-16 13:38:57 +0200

    first upload. add classes

---
+++ LogFiles.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab6
+{
+    internal class LogFiles
+    {
+        public static int Process(string fileName)
+        {
+            using StreamReader sr = new StreamReader(fileName);
+
+            string line1 = sr.ReadLine();
+            string line2 = sr.ReadLine();
+
+            int a = int.Parse(line1);
+            int b = int.Parse(line2);
+
+            checked
+            {
+                return a * b;
+            }
+        }
+
+
+    }
+}
